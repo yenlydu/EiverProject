31564b55d9984d236c3b4ceda1608799bd919dcb
Duong Yen-Ly <yen-ly.duong@epitech.eu>
2021-07-16 18:19:13 +0200

    first init project

---
+++ eiver/eiverApp.swift
@@ -0,0 +1,17 @@
+//
+//  eiverApp.swift
+//  eiver
+//
+//  Created by Duong Yen-Ly on 16/07/2021.
+//
+
+import SwiftUI
+
+@main
+struct eiverApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
